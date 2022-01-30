29660be07bb35ca0d7692c591767bac299ee7476
Liam Smith <liam.s.smith@gmail.com>
2022-01-30 03:55:08 +0000

    linted

--- assertArraysEquals.js
+++ assertArraysEquals.js
@@ -4,16 +4,17 @@
   
 // };
 
+
 const assertArraysEqual = function(array1, array2) {
 
   if (array1.length !== array2.length) {
     return '🛑🛑🛑Asserstion Failed';
   }
-  for (i = 0; i < array1.length; i++){
-    if (array1[i] !== array2[i]){
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
       return '🛑🛑🛑Asserstion Failed';
     }
-  }return '✅✅✅Asserstion Passed';
+  } return '✅✅✅Asserstion Passed';
   
 };
 
@@ -23,11 +24,11 @@ console.log(assertArraysEqual([1, 2, 3], [3, 2, 1])); // => false
 console.log(assertArraysEqual(["1", "2", "3"], ["1", "2", "3"])); // => true
 console.log(assertArraysEqual(["1", "2", "3"], ["1", "2", 3]));// => false
 
-console.log(assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]));// 🟢 
+console.log(assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]));// 🟢
 console.log(assertArraysEqual(["A string"], ["A different string"])); //🔴
 console.log(assertArraysEqual(["A string with a length"], ["Dif string same length"])); //🔴
 console.log(assertArraysEqual([1], ["1"])); //🔴
-console.log(assertArraysEqual([1], [1])); //🟢 
+console.log(assertArraysEqual([1], [1])); //🟢
 console.log(assertArraysEqual([1], [0])); //🔴
 console.log(assertArraysEqual([false], [0])); //🔴
 console.log(assertArraysEqual([true], [true])); //🟢
