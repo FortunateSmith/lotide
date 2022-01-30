e29cdbab1e522988f2d2506b86a7a60a2681988c
Liam Smith <liam.s.smith@gmail.com>
2022-01-30 03:07:26 +0000

    compares two sets of arrays (with numbers) by index

---
+++ eqArrays.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  actual === expected ? console.log(`✅✅✅Asserstion Passed: ${actual} === ${expected}.`)
+    : console.log(`🛑🛑🛑Asserstion Failed: ${actual} !== ${expected}.`);
+  
+};
+
+const eqArrays = function(array1, array2) {
+
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (i = 0; i < array1.length; i++){
+    if (array1[i] !== array2[i]){
+      return false;
+    }
+  }return true;
+  
+};
+
+
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);// => false
+
