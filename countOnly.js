3a5914e7014699a58899c88eb6d9d002792c068d
Liam Smith <liam.s.smith@gmail.com>
2022-02-01 16:23:48 +0000

    counting number of times string appears in object

---
+++ countOnly.js
@@ -0,0 +1,40 @@
+const assertEqual = function(actual, expected) {
+  actual === expected ? console.log(`✅✅✅Asserstion Passed: ${actual} === ${expected}.`)
+    : console.log(`🛑🛑🛑Asserstion Failed: ${actual} !== ${expected}.`);
+  
+};
+
+const countOnly = function(allItems, itemsToCount) {
+  const result = {};
+
+  for (const item of allItems) {
+    console.log(item);
+    if (itemsToCount[item]) {
+      if (result[item]) {
+        result[item] += 1;
+      } else {
+        result[item] = 1;
+      }
+    }
+  }
+  return result;
+};
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
