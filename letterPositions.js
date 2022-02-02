b32a8d9d2403a60886a219b8eeaa3308cae385ee
Liam Smith <liam.s.smith@gmail.com>
2022-02-02 03:08:56 +0000

    return indices of letters from sentence to object

---
+++ letterPositions.js
@@ -0,0 +1,20 @@
+const letterPositions = function(sentence) {
+
+  const results = {};
+  const newString = sentence.split(' ').join('');
+  
+  for (let i = 0; i < newString.length; i++) {
+    let key = newString[i];
+    //if (results[key] === undefined) initializes empty array within results
+    if (results[key] === undefined) {
+      results[key] = [i];
+    } else {
+      //pushes all other instances after fist index of letter
+      results[key].push(i);
+    }
+   
+  }
+  return results;
+};
+
+console.log(letterPositions("lighthouse in the house"));
