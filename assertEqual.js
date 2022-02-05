2de9222bb3c2bb521aa3d2780a183ba2cd7e6744
Liam Smith <liam.s.smith@gmail.com>
2022-02-05 21:12:27 +0000

    corrected assertion function

--- assertEqual.js
+++ assertEqual.js
@@ -1,6 +1,8 @@
+
+//can only compare primitive data types
 const assertEqual = function(actual, expected) {
-  actual === expected ? console.log("✅✅✅Asserstion Passed: " + actual + " === " + expected)
-    : console.log("🛑🛑🛑Asserstion Failed: " + actual + " !== " + expected);
+  actual === expected ? console.log(`✅✅✅Asserstion Passed: ${actual} === ${expected}.`)
+    : console.log(`🛑🛑🛑Asserstion Failed: ${actual} !== ${expected}.`);
   
 };
 
