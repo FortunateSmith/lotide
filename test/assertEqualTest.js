6336ceab357f8db603edcd32df08787d8873acac
Liam Smith <liam.s.smith@gmail.com>
2022-02-16 03:39:14 +0000

    assertEqual test code

---
+++ test/assertEqualTest.js
@@ -0,0 +1,6 @@
+const assertEqual = require('../assertEqual');
+
+assertEqual('Two Pair of Dice', 'Too Paradise');
+assertEqual('Unique New York', 'Unique New York');
+assertEqual(1, 10);
+assertEqual(42, 42);
