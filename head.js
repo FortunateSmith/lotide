764c58283ce44c9a60afd0dc7a5142baa1d97e11
Liam Smith <liam.s.smith@gmail.com>
2022-02-16 01:41:51 +0000

    refactored to import assertEqual module

--- head.js
+++ head.js
@@ -8,11 +8,7 @@ const head = function(array) {
   return firstElement;
 
 };
-//console.log(firstElement);
-// console.log(head([5,6,7]), 5);
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head(["this", "is", "a", "test"]), "this");
-assertEqual(head([42, 21, 0]), 42);
+
+
 
   
