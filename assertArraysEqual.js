6499fdbdea8f8f87e2aefb069e4da42120d60074
Liam Smith <liam.s.smith@gmail.com>
2022-02-16 04:01:41 +0000

    refactored to require eqArrays and export for testing

---
+++ assertArraysEqual.js
@@ -0,0 +1,13 @@
+const eqArrays = require('./eqArrays');
+
+const assertArraysEqual = function(array1, array2) {
+  const result = eqArrays(array1, array2);
+
+  if (result === true) {
+    console.log(`✅✅✅Asserstion Passed: ${array1} === ${array2}.`);
+  } else {
+    console.log(`🛑🛑🛑Asserstion Failed: ${array1} !== ${array2}.`);
+  }
+};
+
+module.exports = assertArraysEqual;
