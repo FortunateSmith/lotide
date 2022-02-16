26f4cb1e362af7e6782e529a20d9397233f188d4
Liam Smith <liam.s.smith@gmail.com>
2022-02-16 20:33:23 +0000

    set for export

--- countLetters.js
+++ countLetters.js
@@ -1,3 +1,5 @@
+const assertObjectsEqual = require("./assertObjectsEqual");
+
 const assertEqual = function(actual, expected) {
   actual === expected ? console.log(`✅✅✅Asserstion Passed: ${actual} === ${expected}.`)
     : console.log(`🛑🛑🛑Asserstion Failed: ${actual} !== ${expected}.`);
@@ -7,7 +9,7 @@ const assertEqual = function(actual, expected) {
 // return unique chars in string
 const countLetters = function(string) {
   const newString = string.split(' ').join('');
-  console.log(newString)
+  console.log(newString);
   // return # of instances of unique letters
   const counter = {};
 
@@ -15,13 +17,25 @@ const countLetters = function(string) {
   for (const character of newString) {
     //console.log(character);
     if (counter[character]) {
-      counter[character] += 1
+      counter[character] += 1;
     } else {
-      counter[character] = 1
+      counter[character] = 1;
     }
   }
-return counter;
+  return counter;
 
-}
-
+};
+module.exports = countLetters;
+assertObjectsEqual(countLetters("lighthouse in the house"), {
+  l: 1,
+  i: 2,
+  g: 1,
+  h: 4,
+  t: 2,
+  o: 2,
+  u: 2,
+  s: 2,
+  e: 3,
+  n: 1,
+}); 
 console.log(countLetters("lighthouse in the house"));
