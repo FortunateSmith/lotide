4476dbef7f8b73e713dc27f1e6715ed6d965452e
Liam Smith <liam.s.smith@gmail.com>
2022-02-16 03:38:21 +0000

    cleaned up

--- assertEqual.js
+++ assertEqual.js
@@ -2,8 +2,8 @@
 //can only compare primitive data types
 const assertEqual = function(actual, expected) {
   actual === expected ? console.log(`✅✅✅Asserstion Passed: ${actual} === ${expected}.`)
-    : console.log(`🛑🛑🛑Asserstion Failed: ${actual} !== ${expected}.`);
-  
+    : console.log(`🛑🛑🛑Asserstion Failed: ${actual} !== ${expected}.`); 
+    
 };
 
 module.exports = assertEqual;
