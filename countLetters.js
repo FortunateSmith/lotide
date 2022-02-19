a6a72f759deda61bfbbf925639403c80daa39b9e
Liam Smith <liam.s.smith@gmail.com>
2022-02-19 02:23:03 +0000

    removed console.logs and added extra assertion test

--- countLetters.js
+++ countLetters.js
@@ -21,17 +21,10 @@ const countLetters = function(string) {
 };
 
 assertObjectsEqual(countLetters("lighthouse in the house"), {
-  l: 1,
-  i: 2,
-  g: 1,
-  h: 4,
-  t: 2,
-  o: 2,
-  u: 2,
-  s: 2,
-  e: 3,
-  n: 1,
+  l: 1, i: 2, g: 1, h: 4, t: 2, o: 2, u: 2, s: 2, e: 3, n: 1,
 });
 
-
+assertObjectsEqual(countLetters('craterellus cornucopioides'), {
+  c: 3, r: 3, a: 1, t: 1, e: 3, l: 2, u: 2, s: 2, o: 3, n: 1, p: 1, i: 2, d: 1
+});
 module.exports = countLetters;
