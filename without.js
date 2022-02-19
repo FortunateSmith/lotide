6fdaf249dcfca750c5c309ef8272ff027e4d7a0f
Liam Smith <liam.s.smith@gmail.com>
2022-02-19 04:13:59 +0000

    refactored to export test to withoutTest.js

--- without.js
+++ without.js
@@ -1,32 +1,4 @@
-const takeUntil = require("./takeUntil");
-
-const eqArrays = function(array1, array2) {
-
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  } return true;
-  
-};
-
-const assertArraysEqual = function(array1, array2) {
-
-  if (array1.length !== array2.length) {
-    return '🛑🛑🛑Asserstion Failed';
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return '🛑🛑🛑Asserstion Failed';
-    }
-  } return '✅✅✅Asserstion Passed';
-  
-};
-
-// Function returns subset of a given array with unwanted items removed.
+// Function returns subset of a given array with unwanted items removed
 
 const without = function(source, itemsToRemove) {
   let result = [];
@@ -38,17 +10,4 @@ const without = function(source, itemsToRemove) {
   
   return result;
 };
-
-module.exports = takeUntil;
-
-console.log(without([1, 2, 3],[1,5])); // => [2, 3]
-console.log(without(["1", "2", "3"], [1, 2, "3"]));
-
-//return source.filter(element => !itemsToRemove.includes(element)) => code Matt demonstrated to do function on a single line.
-
-const words = ["hello", "world", "lighthouse"];
-without(words, ["lighthouse"]); 
-
-// no need to capture return value for this test case
-// Make sure the original array was not altered by the without function
-console.log(assertArraysEqual(words, ["hello", "world", "lighthouse"]));
+module.exports = without;
