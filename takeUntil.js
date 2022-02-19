1c886ea07fdc4ac5a1bf2b1b7456b4efca7143a5
Liam Smith <liam.s.smith@gmail.com>
2022-02-19 03:23:13 +0000

    comment added explains function operation

--- takeUntil.js
+++ takeUntil.js
@@ -1,28 +1,6 @@
-const eqArrays = function(array1, array2) {
+const assertArraysEqual = require('./assertArraysEqual');
 
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  } return true;
-  
-};
-
-
-const assertArraysEqual = function(array1, array2) {
-  const result = eqArrays(array1, array2);
-
-  if (result === true) {
-    console.log(`✅✅✅Asserstion Passed: ${array1} === ${array2}.`);
-  } else {
-    console.log(`🛑🛑🛑Asserstion Failed: ${array1} !== ${array2}.`);
-  }
-};
-
-
+// creates new array with all items up to specified callback item
 const takeUntil = (array, callback) => {
   const results = [];
   for (const item of array) {
@@ -30,23 +8,20 @@ const takeUntil = (array, callback) => {
       return results;
     }
     results.push(item);
-    
-    // while (callback(item) === false) {
-    //   results.push(array[item]);
-    // }
+
   }
 };
 
-// const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-// const results1 = takeUntil(data1, x => x < 0);
-// console.log(results1);
-
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+const expectedResults1 = [1, 2, 5, 7, 2];
+assertArraysEqual(results1, expectedResults1);
 // // console.log('---');
 
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
 const results2 = takeUntil(data2, x => x === ',');
-const expectedResults = ["I've", "been", "to", "Hollywood"];
-assertArraysEqual(results2, expectedResults);
-console.log(results2);
+const expectedResults2 = ["I've", "been", "to", "Hollywood"];
+assertArraysEqual(results2, expectedResults2);
+
 
 module.exports = takeUntil;
