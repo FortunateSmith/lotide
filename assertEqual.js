6a891ab01703e314a5e122c006e2f10b8421dc5c
Liam Smith <liam.s.smith@gmail.com>
2022-02-24 03:50:46 +0000

    add note about testable data type

--- assertEqual.js
+++ assertEqual.js
@@ -2,7 +2,7 @@
 //can only compare primitive data types
 const assertEqual = function(actual, expected) {
   actual === expected ? console.log(`✅✅✅Asserstion Passed: ${actual} === ${expected}.`)
-    : console.log(`🛑🛑🛑Asserstion Failed: ${actual} !== ${expected}.`); 
+    : console.log(`🛑🛑🛑Asserstion Failed: ${actual} !== ${expected}.`);
     
 };
 
