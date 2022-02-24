9af0bb48197bff6076b86afeac89e59cb9135e37
Liam Smith <liam.s.smith@gmail.com>
2022-02-24 03:38:57 +0000

    refactor to test with assertObjectsEqual and add description on line 4

--- letterPositions.js
+++ letterPositions.js
@@ -1,3 +1,4 @@
+const assertObjectsEqual = require('./assertObjectsEqual');
 const letterPositions = function(sentence) {
 
   const results = {};
@@ -17,6 +18,18 @@ const letterPositions = function(sentence) {
   return results;
 };
 
-module.exports = letterPositions;
+// TEST CODE
+assertObjectsEqual(letterPositions("lighthouse in the house"), {
+  l: [ 0 ],
+  i: [ 1, 10 ],
+  g: [ 2 ],
+  h: [ 3, 5, 13, 15 ],
+  t: [ 4, 12 ],
+  o: [ 6, 16 ],
+  u: [ 7, 17 ],
+  s: [ 8, 18 ],
+  e: [ 9, 14, 19 ],
+  n: [ 11 ]
+});
 
-console.log(letterPositions("lighthouse in the house"));
+module.exports = letterPositions;
