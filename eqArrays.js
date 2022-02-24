c879e4f624c794eea57d58d567932e4574c6fc74
Liam Smith <liam.s.smith@gmail.com>
2022-02-24 03:46:58 +0000

    add require assertEqual and move exports.module to last line

--- eqArrays.js
+++ eqArrays.js
@@ -1,3 +1,4 @@
+const assertEqual = require('./assertEqual');
 
 const eqArrays = function(array1, array2) {
 
@@ -12,13 +13,10 @@ const eqArrays = function(array1, array2) {
   
 };
 
-module.exports = eqArrays;
-
-
-
+// TEST CODE
 // assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
 // assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
-
 // assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
 // assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);// => false
 
+module.exports = eqArrays;
