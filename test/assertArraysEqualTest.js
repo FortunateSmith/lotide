25e2abbe6e34a8585a9a00630417cad9d6f2bb6c
Liam Smith <liam.s.smith@gmail.com>
2022-02-24 03:56:37 +0000

    add comment purpose of assertArraysEqualTest file, comment out test code

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,7 +1,8 @@
+// Test File for assertArraysEqual file
 const assertArraysEqual = require('../assertArraysEqual')
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
-assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
-
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);// => false
+// TEST CODE
+// assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true/pass
+// assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false/fail
+// assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true/pass
+// assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);// => false/fail
