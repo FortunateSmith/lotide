fa537f1f45e8b39116bd0e672a82a13d69a2a5d6
Liam Smith <liam.s.smith@gmail.com>
2022-02-24 03:32:05 +0000

    refactor and clean up takeUntil

--- takeUntil.js
+++ takeUntil.js
@@ -12,12 +12,16 @@ const takeUntil = (array, callback) => {
   }
 };
 
+
+// Test Code
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
 const results1 = takeUntil(data1, x => x < 0);
 const expectedResults1 = [1, 2, 5, 7, 2];
 assertArraysEqual(results1, expectedResults1);
-// // console.log('---');
 
+console.log('---');
+
+// Test Code
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
 const results2 = takeUntil(data2, x => x === ',');
 const expectedResults2 = ["I've", "been", "to", "Hollywood"];
