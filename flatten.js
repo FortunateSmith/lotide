3d59d04e516f3bfbdda15a1eb7bafea07d6445dd
Liam Smith <liam.s.smith@gmail.com>
2022-02-24 03:40:40 +0000

    add require assertArraysEqual

--- flatten.js
+++ flatten.js
@@ -1,35 +1,13 @@
-const eqArrays = function(array1, array2) {
+const assertArraysEqual = require('./assertArraysEqual');
 
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  } return true;
-  
+// flattens nested arrays into single array only one level down
+const flatten = function(array) {
+  let flatArray = array.flat();
+  return flatArray;
 };
 
-const assertArraysEqual = function(array1, array2) {
-
-  if (array1.length !== array2.length) {
-    return '🛑🛑🛑Asserstion Failed';
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return '🛑🛑🛑Asserstion Failed';
-    }
-  } return '✅✅✅Asserstion Passed';
-  
-};
-
-
-const flatten = function(array){
-  let flatArray = array.flat()
-  return flatArray
-}
+// TEST CODE
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
+assertArraysEqual(flatten(['one', 12, ['day'], 14, 15, ['soon']]), ['one', 12, 'day', 14, 15, 'soon']);
 
 module.exports = flatten;
-
-console.log(flatten([1, 2, [3, 4], 5, [6]])); // => [1, 2, 3, 4, 5, 6]
