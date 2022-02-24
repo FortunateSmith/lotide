f409bb28dfd0ab65da5e3e9903e1dc30f20b349b
Liam Smith <liam.s.smith@gmail.com>
2022-02-24 03:33:07 +0000

    correct so arrays <= 2 elements return []

--- middle.js
+++ middle.js
@@ -1,20 +1,20 @@
-
+// Function returns new array with middle element(s) of original array
 const middle = function(array) {
   let midElem = [];
   //condition for arrays greater than two that are of odd length
-  if (array.length > 2 && array.length % 2 === 1) {
+  if (array.length <= 2) {
+    return [];
+  } else if (array.length % 2 === 1) {
     midElem.push(array[(array.length - 1) / 2]);
     //condition for arrays greater than two of even length
-  } else if (array.length > 2 && array.length % 2 === 0) {
+  } else if (array.length % 2 === 0) {
     midElem.push(array[(array.length / 2) - 1]);
     midElem.push(array[array.length / 2]);
-  } else {
-    return undefined;
   }
   return midElem;
 };
 
-assertArraysEqual(middle(["red", "orange", "yellow", "green"]), ["orange", "yellow"])
+//assertArraysEqual(middle(["red", "orange", "yellow", "green"]), ["orange", "yellow"])
 
 module.exports = middle;
 
