b90fb1b2d9061b29b3f744eb4fd6690294386fc0
Liam Smith <liam.s.smith@gmail.com>
2022-02-24 03:53:24 +0000

    lint tail test, delete commented out lines

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,23 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle');
 
-assertArraysEqual(middle([7, 3 ,9]),[3]);
-assertArraysEqual(middle([1, 7, 9]), [7]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 'hello', 4]), [2, 'hello']);
+describe('#middle', () => {
+  it('should return 3 for [7, 3 ,9]', () => {
+    assert.deepEqual(middle([7, 3 ,9]),[3]);
+  });
+  it('should return [2, 3 ] for [1, 2, 3, 4]', () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+  it('should return [] for [1, 2]', () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+  it('should return [] for []', () => {
+    assert.deepEqual(middle([]), []);
+  });
+  it('should return ["orange", "yellow"] for ["red", "orange", "yellow", "green"]',() => {
+    assert.deepEqual(middle(["red", "orange", "yellow", "green"]), ["orange", "yellow"]);
+  });
+});
+
+
+
